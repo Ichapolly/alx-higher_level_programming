ae134d9fa87c8515d7e7d442b4612e46822597d2
Ichapolly <pollykarani10@gmail.com>
2024-02-22 15:23:51 +0000

    I play what I call the alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for 2-print_alphabet.py  in range(97-122):
-    print("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z")  
+for 2-print_alphabet.py  in range(97, 122):
+     print(chr(i), end="") 
