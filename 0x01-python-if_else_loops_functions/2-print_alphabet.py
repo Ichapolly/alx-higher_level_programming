4507f6c6b237e092f0f8bf857e7140e660cb4177
Ichapolly <pollykarani10@gmail.com>
2024-02-22 15:17:33 +0000

    aka

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for char in range(26):
-    print("{:s}".format(chr(char + ord("a"))), end="")
+for 2-print_alphabet.py  in range(97-122):
+    print("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z")  
