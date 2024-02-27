c7f3c1c588b617f577fa7b85c96c62fe6decca8a
Ichapolly <pollykarani10@gmail.com>
2024-02-27 09:55:18 +0000

    I sometimes suffer from insomnia

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z')+1):
-    print("{0}-print_alphabet.py".format(chr(letter))) 
+for char in range(ord('a'), ord('z')+1):
+    print("{}: {}".format(chr(char), char))
